20079f06cde7386b8562527680a73c40aa8850a8
Ivan Stroganov <knowhere@gmail.com>
2012-07-30 11:13:29 -0700

    awesome blank rails app created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pollster::Application.config.secret_token = '513ae2df2fd534e2c81efa3ef34ed41afdbb2ca18b866e8535e69b5d92c8eda8bbae4a9291a939b55bd4367cea7e90d918755ae39ae1f996ec4ec212ef5a3e42'
